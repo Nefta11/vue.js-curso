64d49e6a329adf913515cb60f624737b4e54caa8
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-17 04:01:48 -0600

    Agregar funcionalidad para gestionar tareas en el componente ListaDeTareas

--- src/components/ListaDeTareas.vue
+++ src/components/ListaDeTareas.vue
@@ -1,13 +1,21 @@
 <template>
-    <div>
-        <h2>Lista de tareas</h2>
-        <input type="text">
-        <ul>
-            <li>tarea</li>
-        </ul>
-    </div>
+  <div>
+    <h2>Lista de tareas</h2>
+    <input type="text" v-model="nuevaTarea" @keyup.enter="agregarTarea" />
+    <ul>
+      <li v-for="tarea in tareas" :key="tarea">{{ tarea }}</li>
+    </ul>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const tareas = ref([])
+const nuevaTarea = ref('')
+const agregarTarea = () => {
+  tareas.value.push(nuevaTarea.value)
+  nuevaTarea.value = ''
+}
+</script>
 
 <style></style>
