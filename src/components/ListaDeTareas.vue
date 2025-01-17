77d663321f18e5b9f6879c73696ad34c67e1651c
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-17 03:56:18 -0600

    Agregar componente ListaDeTareas y eliminar rutas no utilizadas

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text">
+        <ul>
+            <li>tarea</li>
+        </ul>
+    </div>
+</template>
+
+<script setup></script>
+
+<style></style>
