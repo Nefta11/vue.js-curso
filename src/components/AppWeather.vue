2cbae80bc10465d2ab20c8ebb0bf3d3fe0ac69ba
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-19 14:25:06 -0600

    Actualizar componente AppWeather para mostrar la temperatura desde el store y modificar la función getTemperatura para retornar el objeto de temperatura

--- src/components/AppWeather.vue
+++ src/components/AppWeather.vue
@@ -1,6 +1,15 @@
 <template>
-7°
+  {{ weatherStore.temperatura }}
 </template>
 
 <script setup>
+import { useWeather } from '@/composables/useWeather'
+import { onMounted } from 'vue'
+import { useWeatherStore } from '@/stores/weatherStore'
+
+const weatherStore = useWeatherStore()
+
+onMounted(() => {
+  useWeather()
+})
 </script>
--- src/helpers/getWather.js
+++ src/helpers/getWather.js
@@ -4,7 +4,7 @@ const API_URL =  'https://api.open-meteo.com/v1/forecast?latitude=20.2761&longit
 
 export const getTemperatura = async () => {
   const respuesta = await axios.get(API_URL);
-  const nuevaTemperatura = respuesta.data.hourly.temperature_2m[0].value;
+  const nuevaTemperatura = respuesta.data.hourly.temperature_2m[0];
 
   return nuevaTemperatura;
 }
