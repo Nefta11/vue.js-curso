3f6b7c469f1e03d212514e613d23cc0ebc3c1221
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-18 22:20:07 -0600

    Agregar estilos al componente ListaDeTareas, actualizar MainVue para usar estilos scoped y crear RegistroView como nuevo componente

--- src/modules/listaDeTareas/components/ListaDeTareas.vue
+++ src/modules/listaDeTareas/components/ListaDeTareas.vue
@@ -25,4 +25,57 @@ const borrarTarea = (index) => {
 }
 </script>
 
-<style></style>
+<style scoped>
+div {
+  font-family: Arial, sans-serif;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h2 {
+  text-align: center;
+  color: #333;
+}
+
+input[type='text'] {
+  width: calc(100% - 22px);
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+button {
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #ff1a1a;
+}
+</style>
