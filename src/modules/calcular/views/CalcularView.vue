d94613c4b2d4b8707999f182999921fb943b2636
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-20 21:18:53 -0600

    Agregar validación en CalcularView para mostrar mensajes de error y ocultar ResultadoSection si el valor es menor a 10

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -2,7 +2,9 @@
   <h3>Propiedades Computadas</h3>
   <input type="text" v-model="valor" />
   {{ resultado }}
-  <ResultadoSection v-show="mostrarElemento" :resultado="resultado" />
+  <ResultadoSection v-if="mostrarElemento" :resultado="resultado" />
+  <div v-else-if="isNaN(valor)">Debe ser un numero ptm</div>
+  <div v-else>si es menos a 10 Aqui no se muestra nada</div>
 </template>
 
 <script setup>
