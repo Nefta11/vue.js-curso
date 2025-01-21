00a7562fe98e54e811df41bf0297d5ed335a953d
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-20 21:06:28 -0600

    Corregir enlace en AppSidebar para el componente Calcular; agregar propiedad computada en CalcularView para mostrar el resultado

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -1,14 +1,15 @@
 <template>
-<h3>Propiedades Computadas</h3>
-<input type="text" v-model="valor" />
-{{ valor }}
+  <h3>Propiedades Computadas</h3>
+  <input type="text" v-model="valor" />
+  {{ resultado }}
 </template>
 
 <script setup>
-import { ref} from 'vue'
-const valor = ref('');
-
+import { ref, computed } from 'vue'
+const valor = ref('')
+const resultado = computed(() => {
+  return valor.value * 3
+})
 </script>
-<style scoped>
 
-</style>
+<style scoped></style>
