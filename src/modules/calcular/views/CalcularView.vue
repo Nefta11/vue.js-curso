6d558006781f9b2711c7d6f54fc33fb13813a206
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-20 21:09:45 -0600

    Agregar componente ResultadoSection para mostrar el resultado en CalcularView; actualizar vista con nueva propiedad computada

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -2,10 +2,12 @@
   <h3>Propiedades Computadas</h3>
   <input type="text" v-model="valor" />
   {{ resultado }}
+  <ResultadoSection :resultado="resultado" />
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
+import ResultadoSection from '../components/ResultadoSection.vue';
 const valor = ref('')
 const resultado = computed(() => {
   return valor.value * 3
