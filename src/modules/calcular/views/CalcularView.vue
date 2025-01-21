2e480f559e2151e7cd24090d80780d3cd9bf277c
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-20 21:12:26 -0600

    Agregar lógica para mostrar el componente ResultadoSection solo si el valor es mayor a 10 en CalcularView

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -2,7 +2,7 @@
   <h3>Propiedades Computadas</h3>
   <input type="text" v-model="valor" />
   {{ resultado }}
-  <ResultadoSection :resultado="resultado" />
+  <ResultadoSection v-show="mostrarElemento" :resultado="resultado" />
 </template>
 
 <script setup>
@@ -11,6 +11,10 @@ import ResultadoSection from '../components/ResultadoSection.vue';
 const valor = ref('')
 const resultado = computed(() => {
   return valor.value * 3
+})
+
+const mostrarElemento = computed(() => {
+  return valor.value > 10;
 })
 </script>
 
